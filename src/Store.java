6a00b35b54a143098d577adf2fc1d494c6766a72
Neel Sarathy <nsarathy@purdue.edu>
2022-11-06 15:29:09 -0500

    for pr

--- src/Store.java
+++ src/Store.java
@@ -3,14 +3,6 @@ public class Store {
     // TODO: getters and setters
     // branch Neel
     public Store(String name) {
-        this.name = name;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }

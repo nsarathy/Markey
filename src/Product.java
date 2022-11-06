b1ac674d8c4d8e9d9490eb4b042665565c41900c
Neel Sarathy <nsarathy@purdue.edu>
2022-11-06 15:37:41 -0500

    revert

--- src/Product.java
+++ src/Product.java
@@ -14,4 +14,43 @@ public class Product {
         this.description = description;
     }
 
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Store getStore() {
+        return store;
+    }
+
+    public void setStore(Store store) {
+        this.store = store;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
--- src/Store.java
+++ src/Store.java
@@ -3,6 +3,14 @@ public class Store {
     // TODO: getters and setters
     // branch Neel
     public Store(String name) {
+        this.name = name;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }

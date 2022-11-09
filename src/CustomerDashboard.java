3008afb990afa10a1cad57f07f9ce8a64758fc52
Lewis Park <112414834+lewispark1504@users.noreply.github.com>
2022-11-09 12:05:59 -0500

    basic method implementation
    Couldn't get too far because I don't know the correct format of the CustomerStatistics.txt file yet.

--- src/CustomerDashboard.java
+++ src/CustomerDashboard.java
@@ -1,7 +1,70 @@
+import java.util.*;
+import java.io.*;
+
+
 public class CustomerDashboard {
-    // Format and display CustomerStatistics.txt
+    // TODO: Just display CustomerStatistics.txt
     // CustomerStatistics: Data will include a list of stores by number of products sold
     // Let customer sort by most products sold to the least products sold and vice versa
     // a list of stores by the products purchased by that particular customer. -> Call MarketPlace [viewPurchaseHistory()]
     // Use Scanner to provide an interface for user to do any of the above
+
+
+    public void customerInterface() throws IOException {
+        CustomerDashboard sd = new CustomerDashboard();
+        String[] customerNames = sd.readCustomerStats();
+        int count = customerNames.length;
+        for (int i = 0; i < count; i++) {
+            System.out.println("-------------------");
+            System.out.println(customerNames[i]);
+        }
+    }
+
+    public void displayCustomerStatistics() {
+
+    }
+
+    public void sortProducts() {
+
+    }
+
+    public String[] readCustomerStats() throws FileNotFoundException, IOException {
+        List<String> customerStats = new ArrayList<String>();
+        int count = 0;
+
+        try {
+            File f = new File("CustomerStatistics.txt");
+            FileReader fr = new FileReader(f);
+            BufferedReader bfr = new BufferedReader(fr);
+
+            if (f == null || !(f.exists())) {
+                throw new FileNotFoundException();
+            }
+
+            String line = bfr.readLine();
+
+            while (line != null) {
+                count++;
+                customerStats.add(line);
+                line = bfr.readLine();
+            }
+            String[] customerStatsArr = new String[customerStats.size()];
+            customerStatsArr = customerStats.toArray(customerStatsArr);
+            String[] customerNames = new String[count];
+            for (int i = 0; i < customerNames.length; i++) {
+                ArrayList<String> collectedData = new ArrayList<>(Arrays.asList(customerStatsArr[i].split(";")));
+                customerNames[i] = collectedData.get(0);
+            }
+
+            return customerNames;
+
+        }catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+
+
 }

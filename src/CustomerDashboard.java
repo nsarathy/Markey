86d177c437ad63812ea1942bf6fa4f3048566e51
Lewis Park <112414834+lewispark1504@users.noreply.github.com>
2022-11-10 17:48:04 -0500

    customer dash for option 1  and 3, now function
    still need to implement option 2 and sorting

--- src/CustomerDashboard.java
+++ src/CustomerDashboard.java
@@ -1,3 +1,4 @@
+
 import java.util.*;
 import java.io.*;
 
@@ -14,17 +15,18 @@ public class CustomerDashboard {
 	public static void main(String[] args) throws FileNotFoundException, IOException {
 		CustomerDashboard cd = new CustomerDashboard("testUser");
 		cd.main();
-		
+
 	}
-	
+
 	//constructor
 	public CustomerDashboard(String customerUsername) {
 		this.customerUsername = customerUsername;
 	}
-	
+
 	public void setCustomerUsername(String customerUsername) {
 		this.customerUsername = customerUsername;
 	}
+
 	public String getCustomerUsername() {
 		return customerUsername;
 	}
@@ -39,9 +41,17 @@ public class CustomerDashboard {
 
 	}
 
-	public void sortProducts() {
+	public void sortFullStoreList() {
 		//currently either looking at the full list of all total products sold
 		//and sorting by the amount sold.
+
+		//high to low sort
+
+		//low to high sort
+
+	}
+
+	public void sortPurchaseHistoryList() {
 
 	}
 
@@ -116,7 +126,7 @@ public class CustomerDashboard {
 		String[] specificStoreStr = new String[splitByType.size()];
 		specificStoreStr = splitByType.toArray(specificStoreStr);
 		List<String> splitByStoreAndSales = new ArrayList<>();
-		
+
 		for (int i = 0; i < splitByType.size(); i++) {
 			ArrayList<String> collectedData = new ArrayList<>(Arrays.asList(specificStoreStr[i].split("___")));
 			int storeNum = i + 1;
@@ -129,9 +139,9 @@ public class CustomerDashboard {
 				splitByStoreAndSales.add(collectedData.get(q));
 			}
 		}
-		
+
 		return splitByStoreAndSales;
-		
+
 	}
 
 	// format looks like
@@ -159,31 +169,29 @@ public class CustomerDashboard {
 				storeAndSales.add(splitterOf.get(0));
 				storeAndSales.add(splitterOf.get(1));
 			} else if (!(splitByStoreAndSales.get(i).contains("_"))) {
-				storeAndSales.add("Seller");
 			}
 		}
 		return storeAndSales;
-		
+
 	}
-	
+
 	public List<String> getOnlyStore() throws FileNotFoundException, IOException {
 		List<String> splitByStoreAndSales = byStoreSplitter();
 		String[] specificSalesInfo = new String[splitByStoreAndSales.size()];
 		specificSalesInfo = splitByStoreAndSales.toArray(specificSalesInfo);
 		List<String> onlyStore = new ArrayList<>();
-		
+
 		for (int i = 0; i < splitByStoreAndSales.size(); i++) {
 			if (splitByStoreAndSales.get(i).contains("_")) {
 				List<String> splitterOf = new ArrayList<>(Arrays.asList(specificSalesInfo[i].split("_")));
 				onlyStore.add(splitterOf.get(0));
 			} else if (!(splitByStoreAndSales.get(i).contains("_"))) {
-				onlyStore.add("Seller");
 			}
 		}
 		return onlyStore;
-		
+
 	}
-	
+
 	public List<String> getOnlySales() throws FileNotFoundException, IOException {
 		List<String> splitByStoreAndSales = byStoreSplitter();
 		String[] specificSalesInfo = new String[splitByStoreAndSales.size()];
@@ -195,28 +203,105 @@ public class CustomerDashboard {
 				List<String> splitterOf = new ArrayList<>(Arrays.asList(specificSalesInfo[i].split("_")));
 				onlySales.add(splitterOf.get(1));
 			} else if (!(splitByStoreAndSales.get(i).contains("_"))) {
-				onlySales.add("Seller");
-
 			}
 		}
-				
+
 		return onlySales;
-		
+
 	}
 	
-	
-	
+	public List<Integer> getStoreTotal() throws FileNotFoundException, IOException {
+		List<String> splitByType = typeSplitter();
+		String[] specificStoreStr = new String[splitByType.size()];
+		specificStoreStr = splitByType.toArray(specificStoreStr);
+		List<Integer> numberOfStores = new ArrayList<>();
+		
+		for (int i = 0; i < splitByType.size(); i++) {
+			ArrayList<String> collectedData = new ArrayList<>(Arrays.asList(specificStoreStr[i].split("___")));
+
+			int sizeOfTypes = splitByType.get(i).length() - splitByType.get(i).replaceAll("___", "").length();
+			int wantedNum = sizeOfTypes / 3 + 1;
+
+				numberOfStores.add(wantedNum);
+		}
+		
+		return numberOfStores;
+		
+	}
+
+	public void displayStores() {
+
+	}
+
 	public void main() throws FileNotFoundException, IOException {
 		//this method will contain what the user can do with all these methods, displaying
 		//manipulating and sorting the products with the sort method
+		Scanner input = new Scanner(System.in);
+		boolean repeat = true;
+		final String[] originalSellerNameList = getSellerNames();
+		final List<String> originalStoreList = getOnlyStore();
+		final List<String> originalSaleList = getOnlySales();
+		final List<Integer> storeTotal = getStoreTotal();
+
 		
-		int size = getOnlySales().size();
-		
-		for (int i = 0; i < size; i++) {
-			System.out.println(getOnlySales().get(i));
+		while (repeat) {
+
+			int answer1 = 0;
+
+
+			do {
+				try {
+					System.out.println("*CUSTOMER DASHBOARD*");
+					System.out.println();
+					System.out.println("1. View All Stores");
+					System.out.println("2. View Purchased Stores");
+					System.out.println("3. Exit Customer Dashboard");
+					answer1 = input.nextInt();
+					input.nextLine();
+
+					if (answer1 == 1) {
+						break;
+					} else if (answer1 == 2) {
+						break;
+					} else if (answer1 == 3) {
+						repeat = false;
+						break;
+					} else {
+						System.out.println("Please enter valid choice!");
+						System.out.println();
+					}
+				} catch (InputMismatchException e) {
+					System.out.println("Please enter valid choice!");
+					System.out.println();
+					input.nextLine();
+				}
+			} while (answer1 != 1 || answer1 != 2 || answer1 != 3);
+
+			//first print unsorted original list
+			int update = 0;
+			if (answer1 == 1) {
+				for (int i = 0; i < originalSellerNameList.length; i++) {
+					int count = 0;
+					System.out.println("--------------------");
+					System.out.println("Seller: " + originalSellerNameList[i]);
+					for (int q = 0; q < storeTotal.get(i); q++) {
+						System.out.print(originalStoreList.get(q + update) + ": ");
+						System.out.println(originalSaleList.get(q + update) + " Sales");
+						count++;
+					}
+					update += count;
+				}
+				System.out.println("--------------------");
+				System.out.println();
+				
+
+			}
+
+
+
 		}
-		
+
 
 	}
-	
+
 }

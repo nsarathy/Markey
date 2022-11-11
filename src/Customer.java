69181781cae7e53be8fc4950849b9241dcba4520
Neel Sarathy <111812098+nsarathy@users.noreply.github.com>
2022-11-11 12:39:15 -0500

    display products format

--- src/Customer.java
+++ src/Customer.java
@@ -17,8 +17,16 @@ public class Customer extends Account{
     }
     public void displayPurchases() { //method displaying purchases
         for(int i = 0; i < this.purchases.length; i++) {
+            Product product = this.purchases[i];
+            System.out.printf("\n%s\t\tSold by: %s\t\tPrice: %.2f\tQuantity: %d\nDescription: %s\n",
+                    product.getName(),
+                    product.getStore().getName(),
+                    product.getPrice(),
+                    product.getQuantity(),
+                    product.getDescription());
+            System.out.print(this.purchases[i]);
             if (i == this.purchases.length - 1) {
-                System.out.print(this.purchases[i]);
+
             } else {
                 System.out.print(this.purchases[i] + ", ");
             }
@@ -26,5 +34,16 @@ public class Customer extends Account{
     }
 
     // TODO: equals() checks only
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if ( !(obj instanceof Customer)) {
+            return false;
+        }
+        Customer p = (Customer) obj;
+        return (p.getUsername().equals(this.getUsername()));
+    }
 
 }

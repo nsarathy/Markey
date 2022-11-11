2717c98408ff7dbcce815437191c3e3567ac1f19
Sehonp05 <112426445+Sehonp05@users.noreply.github.com>
2022-11-11 18:09:44 -0500

    Update Cart.java
    No Purchase class > All in Cart class. Please erase Purchase class. 
    Cart has two methods: updateCustomerPurchaseHistory() and updateSellerStatistics().
    When you test in IntelliJ, substitute the file names to the absolute path (right click on text file > Copy Path/Reference > Absolute Path).

--- src/Cart.java
+++ src/Cart.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.ArrayList;
 
 public class Cart {
@@ -9,11 +13,69 @@ public class Cart {
         this.sellerUsernames = sellerUsernames;
         this.customerUsername = customerUsername;
     }
-    // buy() method
-    /*
-    add purchased items to customer history
-    remove products from seller (reduce quantity by the amount purchased by customer)
-    create a Purchase object for each product and call updateSellerStatistics & updateCustomerStatistics
-    Product of index i in productsToBuy, has the seller username at index i in sellerUsernames
-     */
+
+    public void updateCustomerPurchaseHistory() {
+
+        ArrayList<String> list = new ArrayList<>();
+
+        for (int i = 0; i < productsToBuy.size(); i++) {
+            list.add(productsToBuy.get(i).toString() + "$" + sellerUsernames.get(i));
+        }
+
+        String purchaseHistoryLine = customerUsername + ";";
+
+        for (int i = 0; i < list.size(); i++) {
+            if (i == list.size() - 1) {
+                purchaseHistoryLine = purchaseHistoryLine + list.get(i);
+            } else {
+                purchaseHistoryLine = purchaseHistoryLine + list.get(i) + "___";
+            }
+        }
+
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter("CustomerPurchaseHistory.txt", true));
+            bw.write(purchaseHistoryLine);
+            bw.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void updateSellerStatistics() {
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("SellerStatistics.txt"));
+            BufferedWriter bw = new BufferedWriter(new FileWriter("SellerStatistics.txt"));
+            ArrayList<String> list = new ArrayList<>();
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                list.add(line);
+            }
+
+            String sellerName;
+            String customerName;
+            String result = "";
+
+            for (int i = 0; i < productsToBuy.size(); i++) {
+                sellerName = list.get(i).substring(0, list.get(i).indexOf(";"));
+                customerName = list.get(i).substring(list.get(i).lastIndexOf(";") + 1);
+                if (sellerName.equals(sellerUsernames.get(i)) && customerName.equals(customerUsername)) {
+                    result = result + list.get(i).substring(0, list.get(i).lastIndexOf(";"));
+                    result = result + productsToBuy.get(i).toString() + "___;" + customerUsername;
+                } else {
+                    list.add(sellerUsernames.get(i) + ";" + productsToBuy.get(i).toString() + "___;" + customerUsername);
+                }
+            }
+
+            for (int i = 0; i < list.size(); i++) {
+                bw.write(list.get(i));
+            }
+            bw.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }

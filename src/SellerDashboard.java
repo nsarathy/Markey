d0cb0954d6c4a7b348c1eaea1761cf2cb7548171
Neel Sarathy <111812098+nsarathy@users.noreply.github.com>
2022-11-12 23:11:14 -0500

    resetting for final repo

--- src/SellerDashboard.java
+++
@@ -1,7 +0,0 @@
-public class SellerDashboard {
-    /**
-     * TODO: A method to Format and Display SellerStatistics.txt : Data will include a list of customers with the number of items that they have purchased and a list of products with the number of sales.
-     * TODO: Let seller sort items based on most sold to least sold
-     * TODO: Display stores and products and use Scanner to provide an interface for user to do any of the above
-     */
-}

5485922e0963f71112a93865c06b6623b9b15d03
yshin04 <112204696+yshin04@users.noreply.github.com>
2022-11-13 18:40:39 -0500

    java doc add

--- Store.java
+++ Store.java
@@ -1,3 +1,12 @@
+/**
+ * Store
+ *
+ * Store class holding just the name of the store for sellers to create
+ *
+ * @author Yudon Shin, lab sec L-24
+ * 
+ * @version November 13, 2022
+ */
 public class Store {
     private String name;
 

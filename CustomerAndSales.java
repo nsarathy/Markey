3fd0fe1b11f1646f90a5a57c83640d79091f4b4b
Lewis Park <112414834+lewispark1504@users.noreply.github.com>
2022-11-13 18:34:51 -0500

    javadoc update

--- CustomerAndSales.java
+++ CustomerAndSales.java
@@ -1,4 +1,17 @@
 import java.util.List;
+/**
+ * 
+ *  This class is used to store data from SellerDashboard.java
+ *  and stores the values of the customer's names and sales from
+ *  that particular customer. This is mainly used to call from after
+ *  making change to the order of list (sorting).
+ * 
+ * 
+ * @author Lewis Park
+ * @version November 11th, 2022
+ * 
+ * 
+ */
 
 public class CustomerAndSales {
     private final List<Integer> sales;

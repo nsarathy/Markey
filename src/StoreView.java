3e0bf0fa93a45f9af6fe2c8c278e4d438d908656
Lewis Park <112414834+lewispark1504@users.noreply.github.com>
2022-12-01 16:30:00 -0500

    initial commit

---
+++ src/StoreView.java
@@ -0,0 +1,64 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.*;
+
+public class StoreView {
+
+	JFrame frame = new JFrame();
+	
+	JButton highLow;
+	JButton lowHigh;
+	JButton revert;
+	JButton exit;
+	
+	ActionListener actionListener = new ActionListener() {
+		public void actionPerformed(ActionEvent e) {
+			if (e.getSource() == highLow) {
+			}
+			if (e.getSource() == lowHigh) {
+			}
+			if (e.getSource() == revert) {
+			}
+			if (e.getSource() == exit) {
+				frame.dispose();
+			}
+		}
+	};
+	
+	
+	StoreView() {
+		Container content = frame.getContentPane();
+		content.setLayout(new BorderLayout());
+		
+		frame.setSize(300, 700);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setVisible(true);
+        
+        JLabel viewStore = new JLabel("All Stores");
+        highLow = new JButton("High to Low");
+        highLow.addActionListener(actionListener);
+        lowHigh = new JButton("Low to High");
+        lowHigh.addActionListener(actionListener);
+        revert = new JButton("Revert List");
+        revert.addActionListener(actionListener);
+        exit = new JButton("EXIT");
+        exit.addActionListener(actionListener);
+        
+        JPanel nPanel = new JPanel();
+		JPanel cPanel = new JPanel();
+
+		nPanel.add(viewStore);
+		content.add(nPanel, BorderLayout.NORTH);
+		cPanel.add(highLow);
+		content.add(cPanel, BorderLayout.CENTER);
+		cPanel.add(lowHigh);
+		content.add(cPanel, BorderLayout.CENTER);
+		cPanel.add(exit);
+		content.add(cPanel, BorderLayout.CENTER);
+
+	}
+	
+}

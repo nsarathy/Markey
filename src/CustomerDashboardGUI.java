b3e39601e1310fe9d4ed37382e6852a32b1d4b8a
Lewis Park <112414834+lewispark1504@users.noreply.github.com>
2022-12-01 16:30:44 -0500

    added all buttons for front page

--- src/CustomerDashboardGUI.java
+++ src/CustomerDashboardGUI.java
@@ -10,69 +10,72 @@ public class CustomerDashboardGUI extends JComponent implements Runnable {
 	JButton storeView;
 	JButton purchaseHistory;
 	JButton exit;
-	
-	JButton highLowStore;
-	JButton lowHighStore;
-	JButton revertStore;
-	
-	JButton highLowHistory;
-	JButton lowHighHistory;
-	JButton revertHistory;
-	
-	
-	
-	
+
+	JFrame frame = new JFrame("CustomerDashboardGUI");
+
+
 	public static void main(String[] args) {
 		CustomerDashboardGUI cdg = new CustomerDashboardGUI();
 		cdg.main();
 	}
-	
+
 	ActionListener actionListener = new ActionListener() {
 		public void actionPerformed(ActionEvent e) {
-			
+			if (e.getSource() == storeView) {
+				StoreView storeView = new StoreView();
+			}
+			if (e.getSource() == purchaseHistory) {
+				PurchaseHistoryView purchaseHistoryView = new PurchaseHistoryView();
+			}
+			if (e.getSource() == exit) {
+				frame.dispose();
+			}
 		}
 	};
 
-	
-	
+
+
 	public void main() {
 		SwingUtilities.invokeLater(new CustomerDashboardGUI());
 	}
-	
+
 	@Override
 	public void run() {
-		 JFrame frame = new JFrame("CustomerDashboardGUI");
-	        Container content = frame.getContentPane();
-	        content.setLayout(new BorderLayout());
-	        
-	        frame.setSize(500, 200);
-	        frame.setLocationRelativeTo(null);
-	        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-	        frame.setVisible(true);
-	        
-	        
-	        JLabel customerDB = new JLabel("Welcome to Customer Dashboard!");
-	        storeView = new JButton("View All Stores");
-	        storeView.addActionListener(actionListener);
+		Container content = frame.getContentPane();
+		content.setLayout(new BorderLayout());
 
-	        purchaseHistory = new JButton("View Purchase History");
-	        purchaseHistory.addActionListener(actionListener);
-
-	        
-	        JPanel nPanel = new JPanel();
-	        JPanel cPanel = new JPanel();
-	        
-	        nPanel.add(customerDB);
-	        content.add(nPanel, BorderLayout.NORTH);
-	        cPanel.add(storeView);
-	        content.add(cPanel, BorderLayout.CENTER);
-	        cPanel.add(purchaseHistory);
-	        content.add(cPanel, BorderLayout.CENTER);
+		frame.setSize(500, 200);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		frame.setVisible(true);
 
 
-	        
-	        
+		JLabel customerDB = new JLabel("Welcome to Customer Dashboard!");
+		storeView = new JButton("View All Stores");
+		storeView.addActionListener(actionListener);
+
+		purchaseHistory = new JButton("View Purchase History");
+		purchaseHistory.addActionListener(actionListener);
+
+		exit = new JButton("EXIT");
+		exit.addActionListener(actionListener);
+
+		JPanel nPanel = new JPanel();
+		JPanel cPanel = new JPanel();
+
+		nPanel.add(customerDB);
+		content.add(nPanel, BorderLayout.NORTH);
+		cPanel.add(storeView);
+		content.add(cPanel, BorderLayout.CENTER);
+		cPanel.add(purchaseHistory);
+		content.add(cPanel, BorderLayout.CENTER);
+		cPanel.add(exit);
+		content.add(cPanel, BorderLayout.CENTER);
+
+
+
+
 	}
-	
-	
+
+
 }

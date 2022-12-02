35678658e3cfa3e86016d6e031af661a73dc3695
Lewis Park <112414834+lewispark1504@users.noreply.github.com>
2022-12-01 21:11:29 -0500

    full functionality

--- src/CustomerDashboardGUI.java
+++ src/CustomerDashboardGUI.java
@@ -1,8 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.io.IOException;
-import java.net.UnknownHostException;
 
 
 public class CustomerDashboardGUI extends JComponent implements Runnable {
